132fdfb2855383fe9887dd5cefcf6780cdd5dcc6
dodler <avn8068@gmail.com>
2017-11-24 14:15:58 +0300

    xception with all distinct classes

---
+++ helper.py
@@ -0,0 +1,19 @@
+import keras.backend as K
+from keras.preprocessing.image import ImageDataGenerator
+
+def get_generator(path, batch_size):
+    img_size = 299
+    train_gen = ImageDataGenerator(samplewise_center=True,
+                                   samplewise_std_normalization=True)
+    
+    val_gen = ImageDataGenerator(samplewise_center=True,
+                                   samplewise_std_normalization=True)
+    
+    train  = train_gen.flow_from_directory(path + '/train',
+                                           target_size=(img_size, img_size),
+                                          batch_size=batch_size)
+    val = val_gen.flow_from_directory(path + '/val',
+                                      target_size=(img_size, img_size),
+                                     batch_size=batch_size)
+    return train, val
+
